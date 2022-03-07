2396d79079f0dca4bc7e8faffc7df0266e50e02f
roberto <robertopaipo@hotmail.com>
2022-03-07 18:39:29 -0500

    finishing code

--- currency.java
+++ currency.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
 public class currency{
     public static void  converter(){
         // creating a new frame using Jframe
@@ -44,8 +49,71 @@ public class currency{
         b3.setBounds(150,150.60,30);
 
         // adding action listeners
+        b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                // converting to double
+                double d= Double.parseDouble(t1.getText());
+                
+                // converting ruppes to dollars
+                double d1 =(d / 65.25);
 
-    }
+                // getting the string value of the calculated value
+                String str1= String.valueOf(d1);
+
+                //placing it in the text box
+                t2.setText(str1);
+
+            }
+        });
+
+        // adding action listeners
+        b2.addActionListener(new ActionListner(){
+            public  void actionPerformed(ActionEvent e){
+                
+                // converting to double
+                double d2= Double.parseDouble(t2.getText());
+
+                // converting Dollars to rupees
+
+            double d3= (d2 * 65.25);
+
+            // getting the string value of the calculated value
+            
+            String str2 = String.valueOf(d3);
+
+            // placing it in the text box
+
+            t1.setText(str2);
+            }
+        });
+
+        // Default method for closing the frame
+
+        f.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                f.dispose();
+            }
+        });
+        
+        //adding the created objects ot the form
+
+        f.add(l1);
+        f.add(t1);
+        f.add(l2);
+        f.add(t2);
+        f.add(b1);
+        f.add(b2);
+        f.add(b3);
+
+        f.setLayout(null);
+        f.setSize(400,300);
+        f.setVisible(true);
+
+        //drive code 
+        public static void main(String args[])
+        {
+            converter()
+        }
     
     
 }
