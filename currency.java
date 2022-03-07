ee5663bf2a0d7c25a03eb7b429b05aa6a89c231a
roberto <robertopaipo@hotmail.com>
2022-03-07 07:29:23 -0500

    labels

---
+++ currency.java
@@ -0,0 +1,33 @@
+public class currency{
+    public static void  converter(){
+        // creating a new frame using Jframe
+        JFrame f = new JFrame("Converter");
+        
+        // creating two text labels
+        JLabel l1,l2;
+
+        // create two text fields
+        //one for rupee and one for dollar
+        
+        JTextField t1, t2;
+
+        // create three buttons 
+
+        JButton b1, b2 , b3;
+
+        // name the labels and settings
+        // set bounds for labels
+
+        l1= new JLabel("Ruppes:");
+        l1.setBounds(20,40,60,30);
+        l2= new JLabel("Dollars");
+        l2.setBounds(170,40,60,30);
+
+
+        // initialized text fields with 0 by default and setting bound to text field
+        
+
+    }
+    
+    
+}
