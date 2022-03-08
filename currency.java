af18357a599faa0be2bb147d87966f7c2a1a27e3
rob49157 <robertopaipo@hotmail.com>
2022-03-07 20:48:07 -0500

    adding more currencies debuggin

--- currency.java
+++ currency.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class currency{
@@ -95,6 +97,35 @@ public class currency{
                 f.dispose();
             }
         });
+
+        String[] morecurrencies= {"Australia Dollar", "British Pound","Japanese Yen"};
+
+        JFrame jFrame= new JFrame();
+
+        JComboBox<String> jComboBox = new JComboBox<>(morecurrencies);
+        jComboBox.setBounds(80,50,140,20);
+
+        JButton jButton= new JButton("Done");
+        jButton.setBounds(100,100,90,20);
+
+        JLabel JLabel= new JLabel();
+        JLabel.setBounds(90,100,400,100);
+
+        JFrame.add(JButton);
+        JFrame.add(JComboBox);
+        JFrame.add(JLabel);
+
+
+        JFrame.setLayout(null);
+        JFrame.setVisible(true);
+        JFrame.setSize(350,250);
+
+
+        jButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                String selectecurrency= "Currency selected"+ JComboBox.getItemAt(JComboBox.getSelectedIndex());
+            }
+        });
         
         //adding the created objects ot the form
 
@@ -110,6 +141,11 @@ public class currency{
         f.setSize(400,300);
         f.setVisible(true);
     }
+
+
+    // add drop down menu
+    
+
 
         //drive code 
         public static void main(String args[])
--- dropdown.java
+++ dropdown.java
@@ -7,16 +7,17 @@ public class dropdown {
     public static void main(String[] args){
         String[] morecurrencies= {"Australia Dollar", "British Pound","Japanese Yen"};
 
-        JFrame jFrame= new JFrame();
+        JFrame JFrame= new JFrame();
 
-        JComboBox<String> jComboBox = new JComboBox<>(morecurrencies);
-        jComboBox.setBounds(80,50,140,20);
+        JComboBox<String> JComboBox = new JComboBox<>(morecurrencies);
+        JComboBox.setBounds(80,50,140,20);
 
-        JButton jButton= new JButton("Done");
-        jButton.setBounds(100,100,90,20);
+        JButton JButton= new JButton("Done");
+        JButton.setBounds(100,100,90,20);
 
         JLabel JLabel= new JLabel();
         JLabel.setBounds(90,100,400,100);
+        
 
         JFrame.add(JButton);
         JFrame.add(JComboBox);
@@ -28,7 +29,7 @@ public class dropdown {
         JFrame.setSize(350,250);
 
 
-        jButton.addActionListener(new ActionListener(){
+        JButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent event){
                 String selectecurrency= "Currency selected"+ JComboBox.getItemAt(JComboBox.getSelectedIndex());
             }
